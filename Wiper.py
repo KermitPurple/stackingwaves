1aae693db22de95aecc27b1d46031b2cb1c228bc
Shane McDonough <shanemcdman@gmail.com>
2020-03-15 02:43:55 -0700

    Create class Wiper

---
+++ Wiper.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Wiper:
+    def __init__(self, screen, pos, r, n):
+        self.screen = screen
+        self.pos = pos
+        self.r = r
+        self.n = n
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 import os
+from Wiper import Wiper
 os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
 
 pygame.display.init()
 size = 650,650
 screen = pygame.display.set_mode(size)
+wiper = Wiper(screen, (int(size[0]/2), int(size[1]/2)), 200, 1)
 running = True
 while running:
     for event in pygame.event.get():
