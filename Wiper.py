5d2d7b163aa86491b9ce171ac9ae480707457ab3
Shane McDonough <shanemcdman@gmail.com>
2020-03-15 02:50:08 -0700

    Create basic draw

--- Wiper.py
+++ Wiper.py
@@ -1,8 +1,18 @@
 import pygame
+from numpy import sin, cos
 
 class Wiper:
-    def __init__(self, screen, pos, r, n):
+
+    pi = 3.14159265
+
+    def __init__(self, screen, pos, r, n, theta):
         self.screen = screen
         self.pos = pos
         self.r = r
         self.n = n
+        self.theta = theta
+
+    def draw(self):
+        x = self.r * cos(self.theta) + self.pos[0]
+        y = self.r * sin(self.theta) + self.pos[1]
+        pygame.draw.line(self.screen, (255,255,255), self.pos, (x,y))
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
 pygame.display.init()
 size = 650,650
 screen = pygame.display.set_mode(size)
-wiper = Wiper(screen, (int(size[0]/2), int(size[1]/2)), 200, 1)
+wiper = Wiper(screen, (int(size[0]/2), int(size[1]/2)), 200, 1, 0)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill((0,0,0))
+    wiper.draw()
     pygame.display.update()
